960c2fddccb2e52fbc650d0264bb500919ff4915
Logan Dequier <logann.dequier@gmail.com>
2024-05-07 11:12:06 +0200

    Ajout de la gameLoop

--- src/main/java/fr/iut/montreuil/projetfinale/zhellda/controleur/Controleur.java
+++ src/main/java/fr/iut/montreuil/projetfinale/zhellda/controleur/Controleur.java
@@ -2,25 +2,60 @@ package fr.iut.montreuil.projetfinale.zhellda.controleur;
 
 import fr.iut.montreuil.projetfinale.zhellda.modele.Terrain;
 import fr.iut.montreuil.projetfinale.zhellda.vue.VueTerrain;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controleur implements Initializable {
     private Terrain terrain;
+    private Timeline gameLoop;
     @FXML
     private Pane pane;
+    private int temps;
     @FXML
     private TilePane tilePane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         this.terrain = new Terrain();
         new VueTerrain(terrain, tilePane);
+        initAnimation();
+        gameLoop.play();
     }
+    private void initAnimation() {
+        gameLoop = new Timeline();
+        temps=0;
+        gameLoop.setCycleCount(Timeline.INDEFINITE);
+
+        KeyFrame kf = new KeyFrame(
+                // on définit le FPS (nbre de frame par seconde)
+                Duration.seconds(0.017),
+                // on définit ce qui se passe à chaque frame
+                // c'est un eventHandler d'ou le lambda
+                (ev ->{
+                    if(temps==100){
+                        System.out.println("fini");
+                        gameLoop.stop();
+                    }
+                    else if (temps%5==0){
+                        System.out.println("un tour");
+                        leCercle.setLayoutX(leCercle.getLayoutX()+5);
+                        leCercle.setLayoutY(leCercle.getLayoutY()+5);
+
+                    }
+                    temps++;
+                })
+        );
+        gameLoop.getKeyFrames().add(kf);
+    }
+
 }
