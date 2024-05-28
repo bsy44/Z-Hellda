ba5a79f6a65af046a15ed9dddd6e4c2491d5077c
bsy44 <syboubacar444@gmail.com>
2024-05-28 17:14:13 +0200

    merge du marteau

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module fr.iut.montreuil.projetfinale.zhellda {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires com.google.gson;
 
 
     opens fr.iut.montreuil.projetfinale.zhellda to javafx.fxml;
